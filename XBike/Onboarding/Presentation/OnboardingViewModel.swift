477e57d8463b18fa7b03aac8de4f06420888692d
Frank Travieso <frankltp9412@gmail.com>
2025-03-28 19:16:03 -0300

    creating onboarding view model

--- XBike/Onboarding/Presentation/OnboardingViewModel.swift
+++ XBike/Onboarding/Presentation/OnboardingViewModel.swift
@@ -5,3 +5,14 @@
 //  Created by Frank Travieso on 28/03/2025.
 //
 
+class OnboardingViewModel {
+    let steps: [OnboardingStep]
+
+    init() {
+        steps = [
+            OnboardingStep(image: "onboarding_1", title: "Extremely simple to use", buttonTitle: nil),
+            OnboardingStep(image: "onboarding_2", title: "Track your time and distance", buttonTitle: nil),
+            OnboardingStep(image: "onboarding_3", title: "See your progress and challenge yourself!", buttonTitle: "Let's go")
+        ]
+    }
+}
