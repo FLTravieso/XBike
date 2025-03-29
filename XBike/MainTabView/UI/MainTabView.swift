59f3348b6fa7a66a886eaf960f38ba8583eb2697
Frank Travieso <frankltp9412@gmail.com>
2025-03-29 09:53:32 -0300

    adding current ride tab view with toolbar button

--- XBike/MainTabView/UI/MainTabView.swift
+++ XBike/MainTabView/UI/MainTabView.swift
@@ -23,7 +23,7 @@ struct MainTabView: View {
 
     var body: some View {
         TabView {
-            Text("Tab 1")
+            CurrentRideTabView()
                 .tabItem {
                     Image(systemName: "bicycle")
                     Text("Current Ride")
