120e6bb9c83f28348726662fde5176aa8c4759cc
Frank Travieso <frankltp9412@gmail.com>
2025-03-29 00:58:42 -0300

    adding main tab view with styling

---
+++ XBike/MainTabView/UI/MainTabView.swift
@@ -0,0 +1,40 @@
+//
+//  MainTabView.swift
+//  XBike
+//
+//  Created by Frank Travieso on 29/03/2025.
+//
+
+import SwiftUI
+
+struct MainTabView: View {
+    init() {
+        let appearance = UINavigationBarAppearance()
+        appearance.configureWithOpaqueBackground()
+        appearance.backgroundColor = UIColor.orange
+        UINavigationBar.appearance().standardAppearance = appearance
+        UINavigationBar.appearance().scrollEdgeAppearance = appearance
+
+        let tabAppearance = UITabBarAppearance()
+        tabAppearance.backgroundColor = UIColor.white
+        UITabBar.appearance().standardAppearance = tabAppearance
+        UITabBar.appearance().scrollEdgeAppearance = tabAppearance
+    }
+
+    var body: some View {
+        TabView {
+            Text("Tab 1")
+                .tabItem {
+                    Image(systemName: "bike")
+                    Text("Current Ride")
+                }
+
+            Text("Tab 2")
+                .tabItem {
+                    Image(systemName: "gauge")
+                    Text("My Progress")
+                }
+        }
+        .tint(.orange)
+    }
+}
