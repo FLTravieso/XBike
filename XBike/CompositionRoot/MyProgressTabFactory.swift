88fc3ca7118121a4426a6b204c82301570b8b178
Frank Travieso <frankltp9412@gmail.com>
2025-03-30 20:48:48 -0300

    inject same model context for both factories

--- XBike/CompositionRoot/MyProgressTabFactory.swift
+++ XBike/CompositionRoot/MyProgressTabFactory.swift
@@ -5,7 +5,15 @@
 //  Created by Frank Travieso on 30/03/2025.
 //
 
+import SwiftData
+
 class MyProgressTabFactory: CreateMyProgressTab {
+    let context: ModelContext
+
+    init(context: ModelContext) {
+        self.context = context
+    }
+
     func create() -> MyProgressTabView {
         MyProgressTabView(viewModel: MyProgressViewModel())
     }
