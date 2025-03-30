97dc96d6d6c3f474a9db42e7750f5026544eae43
Frank Travieso <frankltp9412@gmail.com>
2025-03-30 19:28:54 -0300

    implementing ride repository

---
+++ XBike/CurrentRide/Data/Repositories/RideRepository.swift
@@ -0,0 +1,31 @@
+//
+//  RideRepository.swift
+//  XBike
+//
+//  Created by Frank Travieso on 30/03/2025.
+//
+import SwiftData
+
+class RideRepository: RidesRepositoryType {
+    private let context: ModelContext
+
+    init(context: ModelContext) {
+        self.context = context
+    }
+
+    func saveRide(with completedRide: CompletedRide) async -> Result<Void, RideDomainError> {
+        let newRide = Ride(startingAddress: completedRide.startingAddress,
+                           finishingAddress: completedRide.finishingAddress,
+                           distance: completedRide.distance,
+                           time: completedRide.time)
+
+        context.insert(newRide)
+
+        do {
+            try context.save()
+            return .success(())
+        } catch {
+            return .failure(.storageError)
+        }
+    }
+}
