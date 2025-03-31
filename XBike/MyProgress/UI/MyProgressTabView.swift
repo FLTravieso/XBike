235a730f570a892b127dbe3dbabedff61ba6c31f
Frank Travieso <frankltp9412@gmail.com>
2025-03-30 21:02:30 -0300

    fetching rides on appear

--- XBike/MyProgress/UI/MyProgressTabView.swift
+++ XBike/MyProgress/UI/MyProgressTabView.swift
@@ -20,5 +20,8 @@ struct MyProgressTabView: View {
         }
         .navigationTitle("My Progress")
         .navigationBarTitleDisplayMode(.inline)
+        .onAppear {
+            viewModel.onAppear()
+        }
     }
 }
